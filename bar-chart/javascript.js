e93ec9c610cd4a75fa40fcd811c8d2bd812e55a1
ozielmaxwell <maxwellozi@gmail.com>
2023-11-07 17:53:40 +0000

    d3 working

--- bar-chart/javascript.js
+++ bar-chart/javascript.js
@@ -1,24 +1,55 @@
-// Load the JSON data
-d3.json("data.json").then(function(data) {
-    // Create an SVG element for the map
-    const svg = d3.select("#uk-map");
+const width = 1000;
+const height = 900;
 
-    // Define a projection (for simplicity, we'll use a basic projection)
-    const projection = d3.geoIdentity().reflectY(true);
+const path = d3.geoPath();
+const projection = d3.geoAlbersUsa().scale(1300).translate([width / 2, height / 2]);
 
-    // Create a path generator based on the projection
-    const path = d3.geoPath().projection(projection);
+const svg = d3.create('svg')
+  .attr('height', height)
+  .attr('width', width);
+const stateBackground = svg.append('path')
+  .attr('fill', '#ddd');
+const stateBorder = svg.append('path')
+  .attr('fill', 'none')
+  .attr('stroke', '#fff')
+  .attr('stroke-linejoin', 'round')
+  .attr('stroke-linecap', 'round');
 
-    // Draw the towns on the map
-    svg.selectAll(".town")
+document.body.appendChild(svg.node());
+
+// Use the fetch API to load the JSON data for the US
+fetch('./us.json')
+  .then(response => response.json())
+  .then(us => {
+    stateBackground.attr('d', path(topojson.feature(us, us.objects.nation)));
+    stateBorder.attr('d', path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+  })
+  .catch(error => {
+    console.error('Failed to load us.json:', error);
+  });
+
+// Use the fetch API to load the JSON data
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Check if data is null or empty
+    if (data && data.length > 0) {
+      // Continue with data processing
+      const stateCapitalElement = svg.selectAll('g')
         .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "town-circle")
-        .attr("cx", d => projection([d.lng, d.lat])[0])
-        .attr("cy", d => projection([d.lng, d.lat])[1])
-        .attr("r", 5); // Set the radius of the town circles
-
-    // You may want to add labels or tooltips for the towns
-    // For example, you can use d3-tip or other techniques to display town names and population.
-});
+        .enter() // Add this to create elements for each data point
+        .append('g')
+        .attr('transform', d => `translate(${projection([d.longitude, d.latitude]).join(",")})`)
+        .append('circle')
+        .attr('r', 5)
+        .append('text')
+        .attr('y', -6)
+        .text(d => d.description);
+      console.log(data);
+    } else {
+      console.error('Loaded data is empty or not an array.');
+    }
+  })
+  .catch(error => {
+    console.error('Failed to load data.json:', error);
+  });
